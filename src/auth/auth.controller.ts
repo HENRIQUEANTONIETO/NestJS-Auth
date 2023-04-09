ed3cbe1797f614fd05a569c19af84ced523db7d1
Henrique <henriquebedendo10@live.com>
2023-04-08 22:21:16 -0300

    Finaliza implementação de refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,33 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
 @Controller('api/auth')
 export class AuthController {
     constructor(private readonly authService: AuthService){}
-    @UseGuards(AuthGuard('local'))
+    @UseGuards(AuthGuard('local')) 
     @Post('/login')
     async login(@Req() req: any){
-        return await this.authService.generateTokens(req.user)  
+        try{
+            return await this.authService.generateTokens(req.user)  
+
+        }catch(error){
+            console.log('aa')
+            return new BadRequestException(error)
+        }
     }
 
     @Post('refresh-token')
     async refreshToken(@Body('refreshToken') ReqRefreshToken: string) {
-        const  user  = await this.authService.verifyRefreshToken(ReqRefreshToken);
-        const { accessToken, refreshToken } = await this.authService.generateTokens(user);
+        try{
+            const  user  = await this.authService.verifyRefreshToken(ReqRefreshToken);
+            const { accessToken, refreshToken } = await this.authService.generateTokens(user);
+    
+            return { accessToken, refreshToken};
 
-        return { accessToken, refreshToken};
+        }catch(error){
+            return new BadRequestException(error)
+        }
 }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,9 @@ import { UsersService } from 'src/app/users/users.service';
 import { compareSync } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { randomBytes } from 'crypto';
-import { verify } from 'jsonwebtoken'
 import { RefreshTokenEntity } from './refreshToken.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { error } from 'console';
 
 @Injectable()
 export class AuthService {
@@ -43,22 +41,32 @@ export class AuthService {
     }
 
     async generateTokens(user: UsersEntity){
-        const payload = {sub: user.id, email: user.email };
-        console.log(user)
-        const accessToken = this.jwtService.sign(payload, { expiresIn: 60 });
-      
-        const refreshToken = randomBytes(64).toString('hex');
-        const refreshExpiresIn = 3600;
-        const expiresAt = new Date(Date.now() + refreshExpiresIn * 1000);
-      
-        const refreshTokenEntity = new RefreshTokenEntity();
-        refreshTokenEntity.token = refreshToken;
-        refreshTokenEntity.userId = user.id;
-        refreshTokenEntity.expiresAt = expiresAt;
-      
-        await this.refreshTokenRepository.save(refreshTokenEntity);
-      
-        return { accessToken, refreshToken };
+        try{
+            const payload = {sub: user.id, email: user.email };
+           
+            const accessToken = this.jwtService.sign(payload, { expiresIn: 60 });
+        
+            const refreshToken = randomBytes(64).toString('hex');
+            const refreshExpiresIn = 3600;
+            const expiresAt = new Date(Date.now() + refreshExpiresIn * 1000);
+        
+            const refreshTokenEntity = new RefreshTokenEntity();
+            refreshTokenEntity.token = refreshToken;
+            refreshTokenEntity.userId = user.id;
+            refreshTokenEntity.expiresAt = expiresAt;
+            
+            const userToken = await this.refreshTokenRepository.findOneBy({userId: user.id})
+            if(userToken){
+                await this.refreshTokenRepository.update({userId: user.id}, refreshTokenEntity)
+            }else{
+                await this.refreshTokenRepository.save(refreshTokenEntity);
+            }
+        
+            return { accessToken, refreshToken };
+        }catch(error){
+            throw error
+        }
+        
     }
 
     async verifyRefreshToken(refreshToken: string){
@@ -67,9 +75,6 @@ export class AuthService {
             if (!refreshToken || !refreshTokenEntity || refreshTokenEntity.expiresAt < new Date()) {
                 throw new UnauthorizedException('Invalid refresh token');
             }
-            
-            console.log('passou')
-            //const { userId } = verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET) as { userId: string };
             
             return await this.userService.findOneOrFail(refreshTokenEntity.userId)
         }catch(error){
